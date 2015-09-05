5411be5bf3f919957fbc6a15bd819c43c9c062b9
Jason Waters <jason@myheck.net>
2015-09-05 14:21:28 -0600

    some basic setup, making webpack-dev-server work with live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
+'use strict';
+var webpack = require('webpack');
+var plugins = [];
+
+if (process.env.PRODUCTION) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin()
+    );
+}
+
 module.exports = {
-  entry: {
-    app: './js/app.js',
-  },
-  output: {
-    path: './build',
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel?stage=0', exclude: [/node_modules/] },
-    ]
-  }
+    entry: {
+        app: './js/app.js',
+    },
+    output: {
+        publicPath: '/build/',
+        path: './build',
+        filename: '[name].js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+        ]
+    },
+    plugins: plugins
 };
